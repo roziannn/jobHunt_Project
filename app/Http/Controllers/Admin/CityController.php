766a43c88197037ce282198209741f808833c168
Firda Rosiana T <firdaroziana10@gmail.com>
2024-05-07 07:53:09 +0700

    fix city CRUD admin locations

--- app/Http/Controllers/Admin/CityController.php
+++ app/Http/Controllers/Admin/CityController.php
@@ -5,11 +5,14 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use App\Models\City;
 use App\Models\Country;
+use App\Models\State;
 use App\Services\Notify;
 use App\Traits\Searchable;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\View\View;
+use Illuminate\Http\Response;
+
 
 class CityController extends Controller
 {
@@ -63,34 +66,51 @@ class CityController extends Controller
     }
 
     /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        //
-    }
-
-    /**
      * Show the form for editing the specified resource.
      */
     public function edit(string $id)
     {
-        //
+        $city = City::findOrFail($id);
+        $countries = Country::all();
+        $states = State::where('country_id', $city->country_id)->get();
+
+        return view('admin.lcoation.city.edit', compact('countries', 'city', 'states'));
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, string $id): RedirectResponse
     {
-        //
+        $request->validate([
+            'country' => ['required', 'integer'],
+            'state' => ['required', 'integer'],
+            'city' => ['required', 'string', 'max:255'],
+        ]);
+
+        $city = City::findOrFail($id);
+        $city->name = $request->city;
+        $city->state_id = $request->state;
+        $city->country_id = $request->country;
+        $city->save();
+
+        Notify::updatedNotification();
+
+        return to_route('admin.cities.index');
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(string $id): Response
     {
-        //
+        try {
+            City::findOrFail($id)->delete();
+            Notify::deletedNotification();
+            return response(['message' => 'success'], 200);
+        } catch (\Exception $e) {
+            logger($e);
+            return response(['message' => 'Something Went Wrong. Please Try Again!'], 500);
+        }
     }
 }
--- resources/views/admin/lcoation/city/edit.blade.php
+++ resources/views/admin/lcoation/city/edit.blade.php
@@ -2,7 +2,7 @@
 @section('contents')
     <section class="section">
         <div class="section-header">
-            <h1>States</h1>
+            <h1>Cities</h1>
         </div>
 
         <div class="section-body">
@@ -10,33 +10,47 @@
         <div class="col-12">
             <div class="card">
                 <div class="card-header">
-                    <h4>Update States</h4>
+                    <h4>Update Cities</h4>
                 </div>
                 <div class="card-body">
-                    <form action="{{ route('admin.states.update', $state->id) }}" method="POST">
+                    <form action="{{ route('admin.cities.update', $city->id) }}" method="POST">
                         @csrf
                         @method('PUT')
                         <div class="row">
-                            <div class="col-md-6">
+                            <div class="col-md-4">
                                 <div class="form-group">
                                     <label for="">Country</label>
                                     <select name="country" id=""
-                                        class="form-control select2 {{ hasError($errors, 'country') }}">
+                                        class="form-control select2 country{{ hasError($errors, 'country') }}">
                                         <option value="">Select</option>
                                         @foreach ($countries as $item)
-                                            <option @selected($state->country_id === $item->id) value="{{ $item->id }}">
+                                            <option @selected($item->id === $city->country_id) value="{{ $item->id }}">
                                                 {{ $item->name }}</option>
                                         @endforeach
                                     </select>
                                     <x-input-error :messages="$errors->get('country')" class="mt-2" />
                                 </div>
                             </div>
-                            <div class="col-md-6">
+                            <div class="col-md-4">
                                 <div class="form-group">
-                                    <label for="">State Name</label>
-                                    <input type="text" class="form-control {{ hasError($errors, 'name') }}"
-                                        name="name" value="{{ old('name', $state->name) }}">
-                                    <x-input-error :messages="$errors->get('name')" class="mt-2" />
+                                    <label for="">States</label>
+                                    <select name="state" id=""
+                                        class="form-control select2 state{{ hasError($errors, 'state') }}">
+                                        <option value="">Select</option>
+                                        @foreach ($states as $item)
+                                            <option @selected($item->id === $city->state_id) value="{{ $item->id }}">
+                                                {{ $item->name }}</option>
+                                        @endforeach
+                                    </select>
+                                    <x-input-error :messages="$errors->get('state')" class="mt-2" />
+                                </div>
+                            </div>
+                            <div class="col-md-4">
+                                <div class="form-group">
+                                    <label for="">City Name</label>
+                                    <input type="text" class="form-control {{ hasError($errors, 'city') }}"
+                                        name="city" value="{{ old('city', $city->name) }}">
+                                    <x-input-error :messages="$errors->get('city')" class="mt-2" />
                                 </div>
                             </div>
                         </div>
@@ -49,3 +63,32 @@
         </div>
     </section>
 @endsection
+
+@push('scripts')
+    <script>
+        $(document).ready(function() {
+            $('.country').on('change', function() {
+
+                let country_id = $(this).val();
+
+                $.ajax({
+                    method: 'GET',
+                    url: '{{ route('admin.get-states', ':id') }}'.replace(":id", country_id),
+                    data: {},
+                    success: function(response) {
+                        let html = '';
+                        //looping the response -> which the State from country_id
+                        $.each(response, function(index, value) {
+                            html += `<option value="${value.id}">${value.name}</option>`
+                        });
+
+                        $('.state').html(html);
+                    },
+                    error: function(xhr, status, error) {
+
+                    }
+                })
+            })
+        })
+    </script>
+@endpush
